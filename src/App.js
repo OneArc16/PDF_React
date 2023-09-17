68bfdb131713a257a4bd65f195896e0934d908e1
Daniel <danielcast244@gmail.com>
2023-09-17 13:06:58 -0500

    EMPENZADO LA CREACION DE FORMATO PDF

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "react-bootstrap"; 
 
 function App() {
+
+  const Menu = () => {
+    return (
+    <nav>
+      <Button>Ver web</Button>
+      <Button>Ver PDF</Button>
+      <Button>Descargar PDF</Button>
+    </nav>
+    )
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Menu />
+      <h1>Hola voy hacer un PDF</h1>
     </div>
   );
 }
